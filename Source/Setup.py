1c30c8490617ec0ef94229f9243bb4207e035d5f
Madff386 <69940651+Madff386@users.noreply.github.com>
2020-11-02 13:15:20 +1100

    Update Setup.py

--- Source/Setup.py
+++ Source/Setup.py
@@ -1,1 +1,25 @@
+import os
 
+
+def find_d(p,q,e):
+    for i = 1:10
+        if 
+
+
+
+
+
+#unzip PKE folder to predetermined location
+
+#move PKE-v2.2.0.exe to desktop
+
+pq list = [49, 51, 53, 59, 61, 67, 71, 73, 79, 83]
+
+e list = [11,13,17,19,23]
+
+P = rand pq list
+Q = rand pq list
+
+E = rand e list
+
+D = find d(p,q,e)
